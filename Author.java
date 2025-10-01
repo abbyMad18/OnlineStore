066d55246c16d2a0ae29f3d8925d4e3a1bce4ef0
abbyMad18 <abbymbruskin@gmail.com>
2025-10-01 12:40:15 -0400

    Update Author.java

--- Author.java
+++ Author.java
@@ -1,4 +1,20 @@
 public class Author
 {
+  
+  private String name;
+  private String dateOfBirth;
 
+  public Author(String n, String d){
+    name = n;
+    dateOfBirth = d;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public String getDateOfBirth(){
+    return dateOfBirth;
+  }
+  
 }
