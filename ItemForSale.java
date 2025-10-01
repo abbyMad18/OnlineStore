fb1a734d4c0bde8924ef1c4d14ab4e41d912bdbd
abbyMad18 <abbymbruskin@gmail.com>
2025-10-01 12:32:58 -0400

    Update ItemForSale.java

--- ItemForSale.java
+++ ItemForSale.java
@@ -1,4 +1,19 @@
 public class ItemForSale
 {
+  private int price;
+  private Object creator;
 
+  public ItemForSale(int p, Object c){
+    price = p;
+    creator = c;
+  }
+
+  public int getPrice(){
+    return price;
+  }
+
+  public Object getCreator(){
+    return creator;
+  }
+  
 }
