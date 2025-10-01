b732610230e71fd40f7a37adf7a3632bd34733c9
abbyMad18 <abbymbruskin@gmail.com>
2025-10-01 12:30:38 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,5 +1,15 @@
 
 public class Book
 {
+  private String title;
+  private String publisher;
 
+  public Book(String t, Author a, int p, String pub){
+    super();
+    title = t;
+    creator = a;
+    price = p;
+    publisher = pub;
+  }
+  
 }
